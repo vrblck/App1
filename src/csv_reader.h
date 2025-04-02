9ea08a14dd80794f7a402f7e3d7c0dd573cb1b95
alexanderveratry <alexdego123@gmail.com>
2025-04-01 22:10:03 -0300

    Añadir Csv_Reader.h
    Añadir Csv_Reader.h

---
+++ src/csv_reader.h
@@ -0,0 +1,28 @@
+// csv_reader.h
+#ifndef CSV_READER_H
+#define CSV_READER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE 1024
+#define MAX_ENTRIES 100
+
+typedef struct {
+    float pizza_id;              // ID de la pizza (puede tener decimales)
+    float order_id;              // ID del pedido (puede tener decimales)
+    char pizza_name_id[50];      // ID del nombre de la pizza
+    float quantity;              // Cantidad vendida (puede tener decimales)
+    char order_date[11];         // Fecha del pedido (formato: dd/mm/yyyy)
+    char order_time[9];          // Hora del pedido (formato: hh:mm:ss)
+    float unit_price;            // Precio unitario
+    float total_price;           // Precio total
+    char pizza_size[2];          // Tamaño de la pizza (S, M, L, etc.)
+    char pizza_category[20];     // Categoría de la pizza (Classic, Veggie, etc.)
+    char pizza_ingredients[200]; // Ingredientes de la pizza
+    char pizza_name[50];         // Nombre de la pizza
+} Venta;
+
+int leer_csv(const char* filename, Venta ventas[], int* total_ventas);
+
+#endif // CSV_READER_H
